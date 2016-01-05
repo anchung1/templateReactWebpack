16f996786fcbc30676700d58694df1bc7b3aa0cc
Tony Chung <anchung@gmail.com>
2016-01-04 23:15:02 -0500

    template

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    //context: __dirname + '/src',
+    entry: {
+        javascript: __dirname + '/src/app.js',
+        html: './index.html',
+        css: './main.css'
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.html$|.css$/,
+                loader: "file?name=[name].[ext]",
+            },
+
+        ]
+    },
+};
